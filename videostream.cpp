3d62410eb2b3491ad5e87a3511aa4b0e5e533a9c
sciatti <sciatti@umich.edu>
2021-06-26 21:17:30 -0400

    some small updates in code

--- videostream.cpp
+++ videostream.cpp
@@ -1,2 +1,23 @@
 #include "videostream.h"
 // Implementation for the videostream class that I will develop using opencv
+
+
+videostream::videostream(float height, float width, float fps)
+{
+    cameraStream.set(cv::CAP_PROP_FRAME_WIDTH, width);
+    cameraStream.set(cv::CAP_PROP_FRAME_HEIGHT, height);
+    cameraStream.set(cv::CAP_PROP_FPS, fps);
+}
+
+void videostream::run()
+{
+    return
+}
+bool videostream::available()
+{
+    return streamQueue.size() > 0;
+}
+cv::OutputArray videostream::getLastImage()
+{
+    return streamQueue.pop();
+} 
--- videostream.h
+++ videostream.h
@@ -8,8 +8,10 @@ class videostream
 {
     public:
         videostream(); // Default Constructor
+        videostream(float height, float width, float fps); // Initialize with fps and resolution arguments
         void run(); // Function To Begin Streaming From The Camera
-
+        bool available(); // Return true if queue is not empty
+        cv::OutputArray getLastImage(); // Return last element in the queue
     private:
         cv::VideoCapture cameraStream; // initialize a video capture object to stream the feed from
         std::deque<cv::OutputArray> streamQueue; // initialize a queue object to store the stream feed in sequential order
